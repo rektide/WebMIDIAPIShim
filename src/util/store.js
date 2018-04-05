d322fb9c0f109e02f4b4b7977df5f2043b3f9e4b
rektide de la faye <rektide@voodoowarez.com>
2018-04-04 22:03:24 -0400

    Store's .keys needed to be a method for spec compliance.
    .keys was being held as an array, but in a Map, .keys() is a function
    that returns a key iterator.

--- src/util/store.js
+++ src/util/store.js
@@ -5,16 +5,19 @@ let idIndex = 0;
 export default class Store {
     constructor() {
         this.store = {};
-        this.keys = [];
+        this._keys = [];
+    }
+    keys(){
+        return this._keys[Symbol.iterator]()
     }
     add(obj) {
         const id = `${new Date().getTime()}${idIndex}`;
         idIndex += 1;
-        this.keys.push(id);
+        this._keys.push(id);
         this.store[id] = obj;
     }
     set(id, obj) {
-        this.keys.push(id);
+        this._keys.push(id);
         this.store[id] = obj;
         return this;
     }
@@ -22,34 +25,34 @@ export default class Store {
         return this.store[id];
     }
     has(id) {
-        return this.keys.indexOf(id) !== -1;
+        return this._keys.indexOf(id) !== -1;
     }
     delete(id) {
         delete this.store[id];
-        const index = this.keys.indexOf(id);
+        const index = this._keys.indexOf(id);
         if (index > -1) {
-            this.keys.splice(index, 1);
+            this._keys.splice(index, 1);
         }
         return this;
     }
     values() {
         const elements = [];
-        const l = this.keys.length;
+        const l = this._keys.length;
         for (let i = 0; i < l; i += 1) {
-            const element = this.store[this.keys[i]];
+            const element = this.store[this._keys[i]];
             elements.push(element);
         }
         return elements;
     }
     forEach(cb) {
-        const l = this.keys.length;
+        const l = this._keys.length;
         for (let i = 0; i < l; i += 1) {
-            const element = this.store[this.keys[i]];
+            const element = this.store[this._keys[i]];
             cb(element);
         }
     }
     clear() {
-        this.keys = [];
+        this._keys = [];
         this.store = {};
     }
 }
